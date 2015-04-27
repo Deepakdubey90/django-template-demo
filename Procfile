a8730ea7602bf8ba494c3d0fe4b6a648464f470e
Deepak <deepakdubey597@gmail.com>
2015-04-27 16:10:26 +0530

    changes in wsgi files

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-#web: python demo/server.py
-web: gunicorn --python demo demo.wsgi
-#web: gunicorn demo.wsgi
+web: gunicorn demo.wsgi
--- demo/wsgi.py
+++ demo/wsgi.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
 
-#from whitenoise.django import DjangoWhiteNoise
-#application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
