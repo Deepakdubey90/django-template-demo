f29b24c5057fcfddb22471c2c0fbd658e6222345
Deepak <deepakdubey597@gmail.com>
2015-04-27 15:29:08 +0530

    wsgi file changes

--- demo/wsgi.py
+++ demo/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
-
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
+
+#from whitenoise.django import DjangoWhiteNoise
+#application = DjangoWhiteNoise(application)
